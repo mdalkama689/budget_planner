285d9691679e37caa1360adc0b136a9d7028171d
mdalkama <mdalkama689@gmail.com>
2025-05-09 16:36:52 +0530

    add info icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import Dashboard from './pages/Dashboard';
 import { BudgetProvider } from './context/BudgetContext';
+import { useEffect, useState } from 'react';
+import MobileShare from './components/share/MobileShare';
+import TabShare from './components/share/TabShare';
 
 function App() {
+    const [deviceType, setDeviceType] = useState<"big" | "small">("big");
+
+  useEffect(() => {
+    const handleResize = () => {
+      setDeviceType(window.innerWidth < 1024 ? "small" : "big");
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <BudgetProvider>
+       {deviceType === "small" ? <MobileShare /> : <TabShare />}
       <Dashboard />
     </BudgetProvider>
   );
